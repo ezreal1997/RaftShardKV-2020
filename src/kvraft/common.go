4e0160f04466f55cac09b11ed3053c8f309831c8
rayzui <1430527996@qq.com>
2020-06-03 14:37:28 +0800

    rpc

--- src/kvraft/common.go
+++ src/kvraft/common.go
@@ -4,8 +4,10 @@ const (
 	OK             = "OK"
 	ErrNoKey       = "ErrNoKey"
 	ErrWrongLeader = "ErrWrongLeader"
+	ErrTimeOut     = "ErrTimeOut"
 )
 
+type msgID int64
 type Err string
 
 // Put or Append
@@ -16,6 +18,8 @@ type PutAppendArgs struct {
 	// You'll have to add definitions here.
 	// Field names must start with capital letters,
 	// otherwise RPC will break.
+	MsgID    msgID
+	ClientID int64
 }
 
 type PutAppendReply struct {
@@ -25,6 +29,8 @@ type PutAppendReply struct {
 type GetArgs struct {
 	Key string
 	// You'll have to add definitions here.
+	MsgID    msgID
+	ClientID int64
 }
 
 type GetReply struct {
